10246e3220c9a792ce86631d62a58ffa7559ccd2
haimd-didi <154407115+haimd-didi@users.noreply.github.com>
2023-12-21 15:07:12 +0200

    HW

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,20 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+	int minLimit = Integer.parseInt(args[0]);
+		int maxLimit = Integer.parseInt(args[1]);
+
+		int randomNum1 =(int) (Math.random()*(maxLimit-minLimit)+minLimit);
+		int randomNum2 =(int) (Math.random()*(maxLimit-minLimit)+minLimit);
+		int randomNum3 =(int) (Math.random()*(maxLimit-minLimit)+minLimit);
+
+		System.out.println(randomNum1);
+		System.out.println(randomNum2);
+		System.out.println(randomNum3);
+
+		int minimal = Math.min(randomNum1, randomNum2);
+		minimal = Math.min(minimal, randomNum3);
+
+		System.out.println("The minimal generated number was "+ minimal);
 	}
 }
