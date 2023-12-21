169ea37c5505566ac863ac4cac4c822faecb66e0
haim-didi <haim.didi@post.runi.ac.il>
2023-12-21 23:28:03 +0200

    home work

--- LinearEq.java
+++ LinearEq.java
@@ -5,11 +5,14 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-        double a = Integer.parseInt(args[0]);
-        double b = Integer.parseInt(args[1]);
-        double c = Integer.parseInt(args[2]);
+        public static void main(String[] args) {
+
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
         double result = (c-b)/a;
 
         System.out.println(a +" * X + "+ b + " = "+ (c));
         System.out.println("X = "+ result);
+        }
 }
