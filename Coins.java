924c222456861162b96b0831a09e27fff978ed2b
haimd-didi <154407115+haimd-didi@users.noreply.github.com>
2023-12-21 14:54:05 +0200

    home work

--- Coins.java
+++ Coins.java
@@ -4,6 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+	int money = Integer.parseInt(args[0]);
+        int quarters = money/25;
+        int cents = money%25;
+        System.out.println("Use "+ quarters +" quarters and "+cents +" cents");	
 	}
 }
