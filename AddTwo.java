a9871484525fc0a5aff7ff81fa08ac3c9b221201
haim-didi <haim.didi@post.runi.ac.il>
2023-12-21 23:30:37 +0200

    home work

--- AddTwo.java
+++ AddTwo.java
@@ -6,7 +6,6 @@ public class AddTwo {
 				int a = Integer.parseInt(args[0]);
                 int b = Integer.parseInt(args[1]);
 				
-                System.out.println("hello");
                 System.out.println(a +" + "+ b + " = "+ (a+b));
 	}
 }
